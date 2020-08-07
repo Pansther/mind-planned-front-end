b782bc22fc896ba56d89f427defd3dcb7bf164f0
Werawit Sirakunwat <werawit_boat@windowslive.com>
2020-08-07 15:09:37 +0700

    edit img path

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,4 @@
 export const PATH = `https://mind-planned-backend.herokuapp.com`;
 // export const PATH = `http://localhost:8080`;
-export const IMG = `${PATH}/static`;
+// export const IMG = `${PATH}/static`;
+export const IMG = `${PATH}`;
